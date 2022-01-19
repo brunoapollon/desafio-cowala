1a8eb4978fe96e1ed74a0d0b214558ea17099cb5
Bruno Lopes <eusoubruno7@gmail.com>
2022-01-19 17:22:51 -0300

    feat: creating section form

--- desafio-cowala-frontend/src/pages/HomePage/index.tsx
+++ desafio-cowala-frontend/src/pages/HomePage/index.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 import Header from '../../components/Header';
 
-import { Container } from './styles';
+import { Container, Content, SectionForm } from './styles';
 
 const HomePage: React.FC = function () {
   return (
     <Container>
       <Header />
+      <Content>
+        <SectionForm />
+      </Content>
     </Container>
   );
 };
--- desafio-cowala-frontend/src/pages/HomePage/styles.ts
+++ desafio-cowala-frontend/src/pages/HomePage/styles.ts
@@ -4,3 +4,15 @@ export const Container = styled.div`
   width: 100%;
   height: 100%;
 `;
+
+export const Content = styled.div`
+  max-width: 68.75em;
+  height: 100%;
+  margin: 4.375em auto;
+`;
+
+export const SectionForm = styled.section`
+  width: 100%;
+  height: 28.75em;
+  background: var(--gray);
+`;
